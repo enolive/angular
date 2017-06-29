065bdfb17d03ccf38039d142afda3579ab74d901
Christoph Welcz <christoph@welcz.de>
2017-06-29 11:38:59 +0200

    use book in book component

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Book} from '../shared/book';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.css']
+  styleUrls: ['./book.component.css'],
 })
 export class BookComponent implements OnInit {
 
-  constructor() { }
+  book: Book;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
