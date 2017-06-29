135f1f26c3c296f89fc22c0d564891ea5dce3c28
Christoph Welcz <christoph@welcz.de>
2017-06-29 15:40:26 +0200

    added book details

---
+++ src/app/book-details/book-details.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+import {Book} from '../shared/book';
+
+@Component({
+    selector: 'app-book-details',
+    templateUrl: './book-details.component.html',
+    styleUrls: ['./book-details.component.css'],
+})
+export class BookDetailsComponent implements OnInit {
+
+    book: Book;
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
