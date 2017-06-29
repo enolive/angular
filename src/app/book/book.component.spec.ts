aad4178ff54276c94e12e4bb7296bdc02a055044
Christoph Welcz <christoph@welcz.de>
2017-06-29 13:26:16 +0200

    fixed tests

--- src/app/book/book.component.spec.ts
+++ src/app/book/book.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
+import {Book} from '../shared/book';
 
 describe('BookComponent', () => {
   let component: BookComponent;
@@ -16,6 +17,7 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+    component.book = {} as Book;
     fixture.detectChanges();
   });
 
