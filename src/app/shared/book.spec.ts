8bcebdeadbf092d535ff663337ab2acba85aebbf
Christoph Welcz <christoph@welcz.de>
2017-06-29 13:48:57 +0200

    introduced rate up/down

--- src/app/shared/book.spec.ts
+++ src/app/shared/book.spec.ts
@@ -1,7 +1,33 @@
 import {Book} from './book';
 
 describe('Book', () => {
-  it('should create an instance', () => {
-    expect(new Book('', '', '')).toBeTruthy();
-  });
+    let book: Book;
+
+    beforeEach(() => book = new Book('', '', ''))
+
+    it('should rate up a book', () => {
+        book.rating = 0;
+        book.rateUp();
+        book.rateUp();
+        expect(book.rating).toBe(2);
+    });
+
+    it('should rate down a book', () => {
+        book.rating = 5;
+        book.rateDown();
+        book.rateDown();
+        expect(book.rating).toBe(3);
+    });
+
+    it('should have a maximal rating of 5', () => {
+       book.rating = 5;
+       book.rateUp();
+       expect(book.rating).toBe(5);
+    });
+
+    it('should have a minimal rating of 0', () => {
+       book.rating = 0;
+       book.rateDown();
+       expect(book.rating).toBe(0);
+    });
 });
--- src/app/shared/book.ts
+++ src/app/shared/book.ts
@@ -1,7 +1,23 @@
 export class Book {
-  constructor(public isbn: string,
-              public title: string,
-              public description: string,
-              public rating = 0) {
-  }
+    constructor(public isbn: string,
+                public title: string,
+                public description: string,
+                public rating = 0) {
+    }
+
+    rateUp() {
+        if (this.rating >= 5) {
+            return;
+        }
+
+        this.rating++;
+    }
+
+    rateDown() {
+        if (this.rating <= 0) {
+            return;
+        }
+
+        this.rating--;
+    }
 }
