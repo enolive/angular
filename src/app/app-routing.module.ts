a341edbb790e605c5bc877473788a03ce6a842db
Christoph Welcz <christoph@welcz.de>
2017-06-29 15:47:40 +0200

    introduced routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {BookDetailsComponent} from './book-details/book-details.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'book/:isbn', component: BookDetailsComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
